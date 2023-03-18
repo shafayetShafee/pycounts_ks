269594e6ab34753ae9e4b443a3af7c3a722c1f36
shafayetShafee <seaborn.sicario@gmail.com>
2023-03-18 23:47:52 +0600

    feat: populated init py with imports of core funcs

--- src/pycounts_ks/__init__.py
+++ src/pycounts_ks/__init__.py
@@ -1,3 +1,7 @@
 # read version from installed package
 from importlib.metadata import version
 __version__ = version("pycounts_ks")
+
+# populate package namespace
+from pycounts_ks.pycounts_ks import count_words
+from pycounts_ks.plotting import plot_words
